d16700f045d8951707664781394065f65118b638
Sophia Crawford <socrawford@chapman.edu>
2020-11-29 18:41:32 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# numbers.sh
+# Sophia Crawford
+echo "Enter a positive number: "
+read N
+i=1
+while [ "$i" -le "$N" ]
+do
+	if [ $(( i % 2 )) -eq 0 ]
+	then
+		echo "$i Even"
+	else
+		echo "$i Odd"
+	fi
+	i=$[i+1]
+done
